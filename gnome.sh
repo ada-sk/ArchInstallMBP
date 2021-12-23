e8f68f461838733eb0a39ce61fa294148297f9d4
ada-sk <95655612+ada-sk@users.noreply.github.com>
2021-12-23 10:55:46 -0600

    Remove unwanted packages

--- gnome.sh
+++ gnome.sh
@@ -4,11 +4,8 @@ SHELL_PATH=$(readlink -f $0 | xargs dirname)
 source ${SHELL_PATH}/bin/global.sh
 
 
-sudo pacman --noconfirm -Syu gnome gnome-extra gnome-tweaks gdm
-
-sudo pacman --noconfirm -Syu awesome-terminal-fonts ttf-liberation  
+sudo pacman --noconfirm -Syu gnome gnome-extra gnome-tweaks gdm 
 sudo pacman --noconfirm -Syu firefox 		# Web Browser
-sudo pacman --noconfirm -Syu alacritty		# Terminal 
 
 info "Zathura PDF and Plug-ins"
 sudo pacman --noconfirm -Syu zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
@@ -22,6 +19,5 @@ sudo pacman --noconfirm -S intel-media-driver libva-intel-driver libvdpau-va-gl 
 
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 
-#sudo systemctl enable gdm
-#sudo systemctl start gdm
-
+sudo systemctl enable gdm
+sudo systemctl start gdm
