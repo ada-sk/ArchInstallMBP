d9c9e66937b3ea7f1ac7d83db231bffc6f048938
ada-sk <95655612+ada-sk@users.noreply.github.com>
2021-12-23 17:56:50 -0600

    only install patched grub

--- install.sh
+++ install.sh
@@ -70,10 +70,10 @@ pacman -S --noconfirm grub efibootmgr
 mkdir -p /boot/efi
 mount /dev/sda1 /boot/efi
 
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
-grub-mkconfig -o /boot/grub/grub.cfg
+#grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+#grub-mkconfig -o /boot/grub/grub.cfg
 
-info "Patching GRUB"
+#info "Patching GRUB"
 cd grub-git
 makepkg -si
 grub-mkconfig -o /boot/grub/grub.cfg
